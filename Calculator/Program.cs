9aad36465ef79ea16c011f5b34671db3d4827b6e
faisal-abed <faisalabed06@gmail.com>
2026-09-23 12:55:21 +0300

    implemented program code

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type the first number followed by the enter key");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type the second number followed by the enter key");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstnumber + secondnumber;
+Console.WriteLine("Adding {0} and {1} gives the answer {2}", firstnumber, secondnumber, result);
+Console.ReadKey();
